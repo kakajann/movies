96c3f8728e955e89caf3326ee0c3de79e93ba447
kakajann <im@kakajann.com>
2021-09-13 03:16:32 +0500

    implement bottom navigation

--- app/src/main/java/com/kakajann/movies/MainActivity.kt
+++ app/src/main/java/com/kakajann/movies/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.kakajann.movies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kakajann.movies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.appBottomNavigator
+        val navController = findNavController(R.id.app_navigator_host)
+        val appBars = setOf(R.id.navigationHome, R.id.navigationSearch, R.id.navigationSettings)
+        val appBarConfiguration = AppBarConfiguration(appBars)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
