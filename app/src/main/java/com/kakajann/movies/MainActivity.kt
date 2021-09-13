3cbfe4bdf72ee0b89d6a9c14a54d4f338b5fd534
kakajann <im@kakajann.com>
2021-09-13 19:32:30 +0500

    implement ViewPager2 to home screen

--- app/src/main/java/com/kakajann/movies/MainActivity.kt
+++ app/src/main/java/com/kakajann/movies/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.kakajann.movies.databinding.ActivityMainBinding
 
@@ -16,15 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupBottomNavigator()
+    }
+
+    private fun setupBottomNavigator() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.appBottomNavigator
         val navController = findNavController(R.id.app_navigator_host)
-        val appBars = setOf(R.id.navigationHome, R.id.navigationSearch, R.id.navigationSettings)
-        val appBarConfiguration = AppBarConfiguration(appBars)
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/kakajann/movies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kakajann/movies/ui/home/HomeFragment.kt
@@ -4,42 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
 import com.kakajann.movies.R
-import com.kakajann.movies.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewPager: ViewPager2 = view.findViewById(R.id.vp2_home)
+
+        val fragments: ArrayList<Fragment> = arrayListOf(
+            MoviesFragment(),
+            TvSeriesFragment()
+        )
+
+        val adapter = ViewPagerAdapter(fragments, this)
+        viewPager.adapter = adapter
     }
 }
