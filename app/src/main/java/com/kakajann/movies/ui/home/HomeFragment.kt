6d9a13f114a9a448404264fea08be4547b70830e
Donnie <im@kakajann.com>
2022-08-31 13:27:31 +0400

    feat: implement view pager in home fragment

--- app/src/main/java/com/kakajann/movies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kakajann/movies/ui/home/HomeFragment.kt
@@ -5,29 +5,58 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kakajann.movies.R
+
+data class FragmentItem(
+    val title: String,
+    val fragment: Fragment
+)
 
 class HomeFragment : Fragment() {
 
+    private lateinit var homeViewPagerAdapter: HomeViewPagerAdapter
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home, container,   false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        val viewPager: ViewPager2 = view.findViewById(R.id.vp2_home)
-
-        val fragments: ArrayList<Fragment> = arrayListOf(
-            MoviesFragment(),
-            TvSeriesFragment()
+        val fragments = setOf<FragmentItem>(
+            FragmentItem(getString(R.string.common_movies), MoviesFragment()),
+            FragmentItem(getString(R.string.common_tv_series), TvSeriesFragment())
         )
 
-        val adapter = ViewPagerAdapter(fragments, this)
-        viewPager.adapter = adapter
+        homeViewPagerAdapter = HomeViewPagerAdapter(fragments, this)
+        viewPager = view.findViewById(R.id.vp2_home)
+        viewPager.adapter = homeViewPagerAdapter
+
+        val tabLayout = view.findViewById<TabLayout>(R.id.tl_home)
+        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
+            tab.text = fragments.elementAt(position).title
+        }.attach()
+    }
+}
+
+class HomeViewPagerAdapter(
+    private val fragments: Set<FragmentItem>,
+    activity: HomeFragment
+) : FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments.elementAt(position).fragment
     }
 }
